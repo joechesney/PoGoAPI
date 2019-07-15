cb2e3f6ef07e9acd19c12bcba3a39c44399ac5e3
Joe Chesney <joedihchesney@gmail.com>
2019-07-15 18:41:20 -0500

    added package request-promise so that my scrape requests are promisified

--- routes/controllers.js
+++ routes/controllers.js
@@ -3,6 +3,7 @@ const { getOnePokemon } = require('./routes.services');
 const allPokemonController = async (req, res, next) => {
 
   const pokemonObject = await getOnePokemon(1);
+  console.log('pokemonObject : ', pokemonObject);
   res.send(pokemonObject)
 }
 
--- routes/routes.services.js
+++ routes/routes.services.js
@@ -1,12 +1,27 @@
-const request = require('request');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
 const getOnePokemon = async (number) => {
-  return {
-    name: 'mankey',
-    number
-  }
+  // 1. Create options object for the request
+  const options = {
+    uri: `https://pokemongo.gamepress.gg/pokemon/${number}`,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36'
+    },
+    transform: (body) => cheerio.load(body)
+  };
+
+  // 2. 
+  const pokemonName = await rp(options)
+  .then(($) => {
+    // get all the stuff out of the html
+    // This is where I would take the pokemon's data and insert it into the database
+    return $('div#page-title h1').text();
+  })
+  .catch ((error) => {
+    console.log('error : ', error);
+  })
+  
+  return { pokemonName };
 }
 
-module.exports = {
-  getOnePokemon
-}
+module.exports = { getOnePokemon }
